f901d1e53216b99e47c30aa4e1acf36cf176d226
Alisha Soedamah <alisha.soedamah@gmail.com>
2026-06-25 16:37:58 +0200

    fix config for github pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig: NextConfig =  {
+  output: 'export',
+  images: {
+    unoptimized: true
+  },
+  basePath: '/your-repo-name',
+  assetPrefix: '/your-repo-name/',
 };
+
+module.exports = nextConfig;
 
 export default nextConfig;
